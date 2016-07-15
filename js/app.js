a9e5e9040b64db771161e19afeb5a018bc102890
Shikhar Dubey <shikhar@e10.in>
2016-07-15 16:18:12 +0530

    Loader edits

--- js/app.js
+++ js/app.js
@@ -1,52 +1,27 @@
-var app = angular.module('app', ['wu.masonry']);
+var app = angular.module('app', ['wu.masonry','ui.router']);
+
+app.config(function($stateProvider, $urlRouterProvider,$locationProvider) {
+  //
+  // For any unmatched url, redirect to /state1
+  $urlRouterProvider.otherwise("/"); 
+  $locationProvider.html5Mode({
+    enabled: true,
+    requireBase: false
+  });
+  //
+  // Now set up the states
+  $stateProvider
+    .state('home', {
+      url: "/",
+      templateUrl: "./views/home.html",
+      controller: "HomeCtrl"
+    })
+});
 
 app.controller('HomeCtrl',['$scope', function($scope) {
 
-
-    /*
-    var links= [{
-                'name' : "First project",
-                'url' : "https://placeimg.com/460/340/nature",
-                'slug' : "first"
-            },{
-                'name' : "First project",
-                'url' : "https://placeimg.com/460/340/people",
-                'slug' : "first"
-            }, {
-                'name' : "Second project",
-                'url' : "https://placeimg.com/230/340/arch",
-                'slug' : "second"
-            },{
-                'name' : "Second project",
-                'url' : "https://placeimg.com/230/340/nature",
-                'slug' : "second"
-            },{
-                'name' : "Third project",
-                'url' : "https://placeimg.com/230/170/any",
-                'slug' : "third"
-            },{
-                'name' : "Third project",
-                'url' : "https://placeimg.com/230/170/arch",
-                'slug' : "third"
-            }];
-
-            var changelink = function() {
-                var randomNumber = Math.floor(Math.random()*6);
-                var color = links[randomNumber];
-                return color;
-            }
-            var add = function() {
-                var data=[];
-                for (var i=0; i<30; i++) {
-                    var d = changelink();
-                    data.push(d);
-                    console.log(JSON.stringify(data))
-                    $scope.projects = dats;
-                }
-            }
-    */
-
     $scope.title = " MARKA | Home";
+    console.log('inside controller');
 
     $scope.projects = [{"name":"Third project","url":"https://placeimg.com/230/170/any","slug":"third"},{"name":"First project","url":"https://placeimg.com/460/340/nature","slug":"first"},{"name":"Second project","url":"https://placeimg.com/230/340/arch","slug":"second"},{"name":"Second project","url":"https://placeimg.com/230/340/arch","slug":"second"},{"name":"First project","url":"https://placeimg.com/460/340/people","slug":"first"},{"name":"Second project","url":"https://placeimg.com/230/340/arch","slug":"second"},{"name":"First project","url":"https://placeimg.com/460/340/nature","slug":"first"},{"name":"Second project","url":"https://placeimg.com/230/340/nature","slug":"second"},{"name":"Second project","url":"https://placeimg.com/230/340/nature","slug":"second"},{"name":"First project","url":"https://placeimg.com/460/340/people","slug":"first"},{"name":"Third project","url":"https://placeimg.com/230/170/arch","slug":"third"},{"name":"Third project","url":"https://placeimg.com/230/170/any","slug":"third"},{"name":"First project","url":"https://placeimg.com/460/340/nature","slug":"first"},{"name":"Third project","url":"https://placeimg.com/230/170/arch","slug":"third"},{"name":"Third project","url":"https://placeimg.com/230/170/arch","slug":"third"},{"name":"Third project","url":"https://placeimg.com/230/170/any","slug":"third"},{"name":"Third project","url":"https://placeimg.com/230/170/arch","slug":"third"},{"name":"Second project","url":"https://placeimg.com/230/340/nature","slug":"second"},{"name":"First project","url":"https://placeimg.com/460/340/nature","slug":"first"},{"name":"First project","url":"https://placeimg.com/460/340/people","slug":"first"},{"name":"First project","url":"https://placeimg.com/460/340/nature","slug":"first"},{"name":"Third project","url":"https://placeimg.com/230/170/arch","slug":"third"},{"name":"Second project","url":"https://placeimg.com/230/340/nature","slug":"second"},{"name":"Second project","url":"https://placeimg.com/230/340/arch","slug":"second"},{"name":"Third project","url":"https://placeimg.com/230/170/any","slug":"third"},{"name":"First project","url":"https://placeimg.com/460/340/people","slug":"first"},{"name":"First project","url":"https://placeimg.com/460/340/people","slug":"first"},{"name":"Third project","url":"https://placeimg.com/230/170/arch","slug":"third"},{"name":"First project","url":"https://placeimg.com/460/340/nature","slug":"first"},{"name":"Third project","url":"https://placeimg.com/230/170/arch","slug":"third"}];
 
---
+++ js/randomiser.js
@@ -0,0 +1,44 @@
+
+
+    /*
+        var links= [{
+                'name' : "First project",
+                'url' : "https://placeimg.com/460/340/nature",
+                'slug' : "first"
+            },{
+                'name' : "First project",
+                'url' : "https://placeimg.com/460/340/people",
+                'slug' : "first"
+            }, {
+                'name' : "Second project",
+                'url' : "https://placeimg.com/230/340/arch",
+                'slug' : "second"
+            },{
+                'name' : "Second project",
+                'url' : "https://placeimg.com/230/340/nature",
+                'slug' : "second"
+            },{
+                'name' : "Third project",
+                'url' : "https://placeimg.com/230/170/any",
+                'slug' : "third"
+            },{
+                'name' : "Third project",
+                'url' : "https://placeimg.com/230/170/arch",
+                'slug' : "third"
+            }];
+
+            var changelink = function() {
+                var randomNumber = Math.floor(Math.random()*6);
+                var color = links[randomNumber];
+                return color;
+            }
+            var add = function() {
+                var data=[];
+                for (var i=0; i<30; i++) {
+                    var d = changelink();
+                    data.push(d);
+                    console.log(JSON.stringify(data))
+                    $scope.projects = dats;
+                }
+            }
+    */
